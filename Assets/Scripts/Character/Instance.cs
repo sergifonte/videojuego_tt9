91982ae278b490ef1639be58382627679d0e20e4
emmacasvil <emma.casassas@estudiantat.upc.edu>
2026-04-04 16:51:23 +0200

    canvi de mida gran -> petit

--- Assets/Scripts/Character/Instance.cs
+++ Assets/Scripts/Character/Instance.cs
@@ -7,6 +7,7 @@ public class Instance : MonoBehaviour
 {
     public GameObject WaxBall;
     public Transform InstancePoint;
+    public GameObject Character; 
 
     private int index = 1; 
 
@@ -21,10 +22,28 @@ public class Instance : MonoBehaviour
             }
             else { return; }
         }
+
+        size(); 
     }
 
     private void Poop()
     {
         var waxBall = Instantiate(WaxBall, InstancePoint.position, InstancePoint.rotation);
+    }
+
+    private void size()
+    {
+        switch (index)
+        {
+            case 0:
+                Character.transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
+                break;
+            case 1: 
+                Character.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+                break;
+            case 2:
+                Character.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+                break; 
+        }
     }
 }
