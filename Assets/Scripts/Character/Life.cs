abefbc1098274afe2729aed84ab4f8926f32de90
emmacasvil <emma.casassas@estudiantat.upc.edu>
2026-06-01 10:56:10 +0200

    barra de vida

--- Assets/Scripts/Character/Life.cs
+++ Assets/Scripts/Character/Life.cs
@@ -2,43 +2,27 @@ using UnityEngine;
 
 public class Life : MonoBehaviour
 {
-    /*SCRIPT PER LES BOLES DE FOC QUE LI BAIXEN LA VIDA AL JUGADOR
-      Detecta tant per col·lisió sòlida com per Trigger (transparent)*/
-
-    public float damageAmount = 1.0f;
-    private bool isColliding = false;
-
+    public float damageAmount = 0.1f;
     private PlayerHealth playerHealth;
 
     void Start()
     {
         playerHealth = GetComponent<PlayerHealth>();
-    }
 
-    void Update() //Això para el joc si el jugador es mor
-    {
-        if (playerHealth._currentHealth <= 0)
+        if (playerHealth == null)
         {
-            Time.timeScale = 0f;
+            Debug.LogError("No s'ha trobat el script PlayerHealth en aquest objecte!");
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.CompareTag("fireBall"))
+        if (other.gameObject.CompareTag("fireBall") && playerHealth != null)
         {
             playerHealth.TakeDamage(damageAmount);
-            isColliding = true;
         }
     }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.CompareTag("fireBall"))
-        {
-            isColliding = false;
-        }
-    }
+}
 
     //Script anterior
     //private void OnCollisionEnter(Collision other)
@@ -57,4 +41,3 @@ public class Life : MonoBehaviour
     //        isColliding = false;
     //    }
     //}
-}
