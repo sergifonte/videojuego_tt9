a2830722a5e0302a63bcce7b12561479ba7c5072
emmacasvil <emma.casassas@estudiantat.upc.edu>
2026-06-01 11:58:32 +0200

    barra de vida

--- Assets/Scripts/Character/Life.cs
+++ Assets/Scripts/Character/Life.cs
@@ -2,59 +2,24 @@ using UnityEngine;
 
 public class Life : MonoBehaviour
 {
-    /*SCRIPT PER LES BOLES DE FOC QUE LI BAIXEN LA VIDA AL JUGADOR
-      Detecta tant per col·lisió sòlida com per Trigger (transparent)*/
-
-    public float damageAmount = 1.0f;
-    private bool isColliding = false;
-
+    public float damageAmount = 0.1f; 
     private PlayerHealth playerHealth;
 
     void Start()
     {
         playerHealth = GetComponent<PlayerHealth>();
-    }
 
-    void Update() //Això para el joc si el jugador es mor
-    {
-        if (playerHealth._currentHealth <= 0)
+        if (playerHealth == null)
         {
-            Time.timeScale = 0f;
+            Debug.LogError("No s'ha trobat el script PlayerHealth en aquest objecte!");
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.CompareTag("fireBall"))
+        if (other.gameObject.CompareTag("fireBall") && playerHealth != null)
         {
             playerHealth.TakeDamage(damageAmount);
-            isColliding = true;
         }
     }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.CompareTag("fireBall"))
-        {
-            isColliding = false;
-        }
-    }
-
-    //Script anterior
-    //private void OnCollisionEnter(Collision other)
-    //{
-    //    if (other.gameObject.CompareTag("fireBall"))
-    //    {
-    //        playerHealth.TakeDamage(damageAmount);
-    //        isColliding = true;
-    //    }
-    //}
-
-    //private void OnCollisionExit(Collision other)
-    //{
-    //    if (other.gameObject.CompareTag("fireBall"))
-    //    {
-    //        isColliding = false;
-    //    }
-    //}
 }
--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,19 +1,16 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    [SerializeField] private Image _healthsprites;
+    [SerializeField] private Image _healthsprites; // Arrossega aquí la imatge amb el "Image Type: Filled"
 
+    // Netegem els paràmetres, ara el mètode només fa cas al que li envien
     public void UpdateHealthBar(float maxHealth, float currentHealth)
     {
-        _healthsprites.fillAmount = currentHealth / maxHealth;
-    }
-
-    void Update()
-    {
-        
+        if (_healthsprites != null && maxHealth > 0)
+        {
+            _healthsprites.fillAmount = currentHealth / maxHealth;
+        }
     }
 }
