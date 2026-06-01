ed07268bbde4ce3fae199781e54989524af5361d
Xeniaa007 <xenia.jordan@estudiantat.upc.edu>
2026-06-01 21:27:52 +0200

    JOC 100% ACABAT

--- Assets/Scripts/SpawnPoint/Spawner.cs
+++ Assets/Scripts/SpawnPoint/Spawner.cs
@@ -3,30 +3,26 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public Transform spawnPoint; //llegim el GameObject buit del SpawnPoint al inspector
+    public Transform spawnPoint; 
 
-    private void OnTriggerEnter(Collider Ground) //el gameObject té el trigger activat perquè ho llegeixi el codi
+    private void OnTriggerEnter(Collider Ground) 
     {
-        if (Ground.CompareTag("Player")) //el tag està enllaçat a la part de dalt del inspector al GameObject "Character"
-            //comprovem que el objecte que ha caigut al buit sigui el player
+        if (Ground.CompareTag("Player")) 
+            //es comprova que el objecte que ha caigut al buit sigui el player
         {
-            CharacterController Control_jugador = Ground.GetComponent<CharacterController>(); //llegim el character controler que el Character té
-            //li he assignat el nom de la variable "Control_jugador"
+            CharacterController Control_jugador = Ground.GetComponent<CharacterController>(); 
 
-            //perquè el usuari eviti de moure el player quan aquest ja ha tocat el buit (així evitem bugs raros del futur)
-            if (Control_jugador != null) Control_jugador.enabled = false; //desactivem els controls perquè el usuari NO el pugui moure
+            if (Control_jugador != null) Control_jugador.enabled = false; 
 
-            //respawnegem el jugador en la posició i rotació tal i com està el SpawnPoint
             Ground.transform.position = spawnPoint.position;
             Ground.transform.rotation = spawnPoint.rotation;
 
-            if (Control_jugador != null) Control_jugador.enabled = true; //tornem a activar els permisos perquè el usuari pugui jugar
+            if (Control_jugador != null) Control_jugador.enabled = true; 
 
-            Debug.Log("El jugador ha respawnejat correctament!"); //enviem un debug a la pantalla per comprovar que tot funcioni correctament
+            Debug.Log("El jugador ha respawnejat correctament!"); 
         }
         else
-        { //en cas de que hi hagi algun problema sortirà això:
-          // Això sortirà si cau qualsevol altra objecte (un cub, una bala, etc.)
+        { 
             Debug.Log("PROBLEMA EN EL RESPAWN: L'objecte no és el Player");
         }
     }
